12be0870d606e8c41a231ebd437334542b6d21cb
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2025-04-14 18:24:37 -0400

    feat: rounded border for images

--- main.ts
+++ main.ts
@@ -1,21 +1,86 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { readFileSync } from "fs";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Style {
+	name: string;
+	css: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface ImageStyleSettings {
+	borderRadius: string;
+	styles: Style[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ImageStyleSettings = {
+	borderRadius: 'sm',
+	styles: [
+		{ "name": "rounded-none", "css": "image-style-rounded-none" },
+		{ "name": "rounded-xs", "css": "image-style-rounded-xs" },
+		{ "name": "rounded-sm", "css": "image-style-rounded-sm" },
+		{ "name": "rounded-md", "css": "image-style-rounded-md" },
+		{ "name": "rounded-lg", "css": "image-style-rounded-lg" },
+		{ "name": "rounded-xl", "css": "image-style-rounded-xl" },
+		{ "name": "rounded-2xl", "css": "image-style-rounded-2xl" },
+		{ "name": "rounded-3xl", "css": "image-style-rounded-3xl" },
+		{ "name": "rounded-4xl", "css": "image-style-rounded-4xl" },
+	]
+}
+
+export default class ImageStyle extends Plugin {
+	settings: ImageStyleSettings;
 
 	async onload() {
 		await this.loadSettings();
 
+		// const onUpdate = () => {
+		// 	const file = this.app.workspace.getActiveFile()
+		// 	if (file) {
+		// 		const imagesDivs = this.app.workspace.containerEl.getElementsByClassName("image-embed")
+		// 		console.log(imagesDivs)
+		// 		Array.from(imagesDivs).forEach((imageContainerDiv: any) => {
+		// 			const img = imageContainerDiv.children[0]
+		// 			if (img.tagName === "IMG") {
+		// 				const imageBorderRadiusClassName = "image-style-rounded-" + this.settings.borderRadius
+		// 				img.classList.add(imageBorderRadiusClassName)
+		// 			}
+		// 		})
+		// 	}
+		// }
+
+		// this.app.workspace.on("editor-change", onUpdate)
+
+		// this.app.workspace.onLayoutReady(() => {
+		// 	const file = this.app.workspace.getActiveFile()
+		// 	if (file) {
+		// 		const imagesDivs = this.app.workspace.containerEl.getElementsByClassName("image-embed")
+		// 		console.log(imagesDivs)
+		// 		Array.from(imagesDivs).forEach((imageContainerDiv: any) => {
+		// 			const img = imageContainerDiv.children[0]
+		// 			if (img.tagName === "IMG") {
+		// 				const imageBorderRadiusClassName = "image-style-rounded-" + this.settings.borderRadius
+		// 				img.classList.add(imageBorderRadiusClassName)
+		// 			}
+		// 		})
+		// 	}
+		// })
+
+		const file = this.app.workspace.getActiveFile()
+		if (file) {
+			// const wimages = this.app.workspace.containerEl.getElementsByTagName("img")
+			// console.log(wimages)
+			const images = this.app.workspace.containerEl.getElementsByClassName("image-embed")
+			console.log(images)
+			Array.from(images).forEach((imageContainerDiv: any) => {
+				const img = imageContainerDiv.children[0]
+				if (img.tagName === "IMG") {
+					const imageBorderRadiusClassName = "image-style-rounded-" + this.settings.borderRadius
+					img.classList.add(imageBorderRadiusClassName)
+				}
+			})
+		}
+		
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
@@ -24,27 +89,6 @@ export default class MyPlugin extends Plugin {
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -56,7 +100,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						console.log(checking);
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,16 +110,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ImageStyleSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,44 +126,43 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class ImageStyleSettingTab extends PluginSettingTab {
+	plugin: ImageStyle;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ImageStyle) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		const APP_TITLE = this.plugin.manifest.name + " " + this.plugin.manifest.version
+
+		containerEl.createEl("h1", { text: APP_TITLE })
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Rounded Borders")
+			.setDesc("Select the border radius of images.")
+			.addDropdown((text) =>
+				text
+					.addOption("none", "No border")
+					.addOption("xs", "Extra Small")
+					.addOption("sm", "Small")
+					.addOption("md", "Medium")
+					.addOption("lg", "Large")
+					.addOption("xl", "Extra Large")
+					.addOption("2xl", "2XL")
+					.addOption("3xl", "3XL")
+					.addOption("4xl", "4XL")
+					.setValue(this.plugin.settings.borderRadius)
+					.onChange(async (value) => {
+						this.plugin.settings.borderRadius = value
+						await this.plugin.saveSettings()
+					})
+			)
 	}
 }
